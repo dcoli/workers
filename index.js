e6cbd79f6f799f85bbac3f1cc2c842f894263d97
Colin MacAllister <colin.macallister@generalassemb.ly>
2020-08-25 15:59:49 -0400

    working with server-side API calls

---
+++ index.js
@@ -0,0 +1,45 @@
+// const http = require('http')
+
+const express = require("express")
+const app = express()
+const router = express.Router()
+
+const path = require("path")
+const fetch = require("node-fetch")
+
+const hostname = "127.0.0.1"
+const port = 3000
+express.static("./src")
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/src/index.html"));
+});
+
+// https://api-4ff4f23f.eu.v2.we-stats.com/api/v6/score
+app.get("/getScore", function (req, res) {
+    body = { "action": "getScore", "activityAmountTotal": "400.00", "activityName": "GUEST_PAYMENT", "customerID": "dummy", "customerSessionID": "iframecsid", "IP": req.ip, "platformType": "WEB", "solution": "AO", "uuid": "no_uuid", "yearOfBirth": 1970 }
+
+    fetch('https://api-4ff4f23f.eu.v2.we-stats.com/api/v6/score', {
+        method: 'post',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(json => res.send(`<pre style="font-size:smaller">${JSON.stringify(json, null, 3)}</pre>`));
+})
+
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname + "/src/" + req.path))
+});
+
+app.listen(port);
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
