cf56bed0efa34123cd936d26a2c6beff31caf9a0
Colin MacAllister <colin.macallister@generalassemb.ly>
2020-09-24 18:58:09 -0400

    ready for heroku

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // const http = require('http')
 
 const express = require("express")
-const app = express()
-const router = express.Router()
-
 const path = require("path")
 const fetch = require("node-fetch")
 
+// const hostname = "1213c49718ca.ngrok.io"
 const hostname = "parent.magnoliatree.net"
-const apiUrl = "https://api-4ff4f23f.eu.v2.we-stats.com/api/v6/score"
+// const apiUrl = "https://api-4ff4f23f.eu.v2.we-stats.com/api/v6/score"
+const apiUrl = "https://api-dab0bb57.us.v2.we-stats.com/api/v6/score?cid=icemantest"
+const app = express()
+const router = express.Router()
 
 express.static("./src")
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/src/index.html"));
+  res.sendFile(path.join(__dirname + "/src/index.html"))
 });
 
 app.get("/getScore", function (req, res) {
@@ -25,7 +26,7 @@ app.get("/getScore", function (req, res) {
         headers: { 'Content-Type': 'application/json' },
     })
     .then(res => res.json())
-    .then(json => res.send(`<pre style="font-size:smaller">${JSON.stringify(json, null, 3)}</pre>`));
+    .then(json => res.send(`<pre style="font-size:smaller">${JSON.stringify(json, null, 3)}</pre>`))
 })
 
 app.use(function (req, res) {
@@ -34,8 +35,8 @@ app.use(function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 80;
+  port = 3000;
 }
 app.listen(port);
 
-console.log(`Server running at http://${hostname}:${port}/`);
+console.log(`Server running at http://${hostname}:${port}/`)
